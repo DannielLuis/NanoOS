10c5e264f4945ba8be68604651ab6d5f50cc191b
Daniel Luis <danniellcontato@gmail.com>
2026-03-24 19:31:18 -0300

    limpeza e reorganização boot

--- boot.asm
+++ boot.asm
@@ -1,3 +1,5 @@
+%include "boot_params.inc"
+
 [org 0x7C00]
 [bits 16]
 
@@ -16,40 +18,26 @@ start:
 
     mov [BOOT_DRIVE], dl
 
+; =========================
 ; print msg
+; =========================
     mov si, msg
     call print
 
 
 ; =========================
-; carregar stage2
+; carregar stage2 (loader)
 ; =========================
 
     xor ax, ax
     mov es, ax          ; <<< IMPORTANTE
 
     mov bx, 0x8000      ; destino
-    mov dh, 5           ; setores para ler
-   ;; mov dh, 10           ; setores para ler
-   ;; mov dh, 2           ; setores para ler
+   ; mov dh, 5           ; setores para ler
+    mov dh, LOADER_SECTORS
 
     call disk_load
 
-
-;mov ax, 0x8000
-;mov ds, ax
-;mov si, 0
-;lodsb
-;mov ah,0x0E
-;int 0x10
-mov si, test_msg
-call print
-
-mov bx, 0x8000
-mov al, [bx]
-
-mov ah, 0x0E
-int 0x10
 
 ; =========================
 ; testar se carregou
@@ -60,20 +48,17 @@ int 0x10
 
 
 ; =========================
+; passar info para loader
+; =========================
+
+    mov [0x7E00], dl
+    mov byte [0x7E01], KERNEL_SECTOR
+
+
+; =========================
 ; pular para stage2
 ; =========================
-;mov ax, 0x8000
-;mov ds, ax
-;mov si, 0
-;lodsb
-;mov ah,0x0E
-;int 0x10
-
-mov [0x7E00], dl         ; BOOT_DRIVE
-mov byte [0x7E01], 7     ; KERNEL_SECTOR (TEMPORÁRIO vindo do script)
-
     jmp 0x0000:0x8000
-   ;jmp 0x0800:0x0000
 
 
 
@@ -88,15 +73,8 @@ disk_load:
     push dx
     push es
 
-    xor ax, ax
-    mov es, ax
-
-    mov bx, 0x8000
-
     mov ah, 0x02
-   ;; mov al, dh
-   ;; mov al, 1
-    mov al, 5
+    mov al, dh
 
     mov ch, 0
     mov cl, 2
@@ -114,24 +92,6 @@ disk_load:
     pop ax
 
     ret
-
-;disk_load:
-
-  ;  pusha
-
-  ;  mov ah, 0x02    ; INT13 read
-  ;  mov al, dh      ; setores
-  ;  mov ch, 0       ; cilindro
-  ;  mov cl, 2       ; setor começa em 2
-  ;  mov dh, 0       ; cabeça
-  ;  mov dl, [BOOT_DRIVE]
-
-  ;  int 0x13
-
-  ;  jc disk_error
-
-   ; popa
-  ;  ret
 
 
 disk_error:
@@ -158,16 +118,6 @@ print:
 
     jmp .loop
 
-;.next:
-
- ;   lodsb
-  ;  or al, al
-   ; jz .done
-
-  ;  mov ah, 0x0E
-  ;  int 0x10
-
-  ;  jmp .next
 
 .done:
     ret
@@ -181,7 +131,7 @@ BOOT_DRIVE db 0
 msg db "NanoOS boot", 0
 ok_msg db " stage2 loaded",0
 msg_error db "Disk error",0
-test_msg db " check:",0
+
 
 times 510-($-$$) db 0
 dw 0xAA55
