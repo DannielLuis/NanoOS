f4b2f0dd2b29c009b35cf782e56cf3750c41bd8e
Daniel Luis <danniellcontato@gmail.com>
2026-04-04 02:02:34 -0300

    atualiza driver ata

--- src/kernel/drivers/ata.asm
+++ src/kernel/drivers/ata.asm
@@ -10,6 +10,72 @@ ata_read_sector:
 
     pushad
 
+    mov ebx, eax   ; salvar LBA
+
+.wait_bsy:
+    mov dx, 0x1F7
+    in al, dx
+    test al, 0x80
+    jnz .wait_bsy
+
+    ; setores = 1
+    mov dx, 0x1F2
+    mov al, 1
+    out dx, al
+
+    ; LBA 0-7
+    mov eax, ebx
+    mov dx, 0x1F3
+    out dx, al
+
+    ; LBA 8-15
+    mov dx, 0x1F4
+    mov al, ah
+    out dx, al
+
+    ; LBA 16-23
+    shr eax, 16
+    mov dx, 0x1F5
+    out dx, al
+
+    ; LBA 24-27 + drive
+    mov dx, 0x1F6
+    mov al, 0xE0
+    or al, ah
+    out dx, al
+
+    ; comando READ
+    mov dx, 0x1F7
+    mov al, 0x20
+    out dx, al
+
+.wait_drq:
+    in al, dx
+    test al, 0x08
+    jz .wait_drq
+
+    ; checar erro
+    test al, 0x01
+    jnz .error
+
+    ; ler 256 words (512 bytes)
+    mov dx, 0x1F0
+    mov ecx, 256
+
+.read:
+    in ax, dx
+    mov [edi], ax
+    add edi, 2
+    loop .read
+
+    popad
+    ret
+
+.error:
+    popad
+    ret
+
+
 .wait:
     mov dx, 0x1F7
     in al, dx
@@ -45,23 +111,23 @@ ata_read_sector:
     mov al, 0x20
     out dx, al
 
-.wait2:
-    in al, dx
-    test al, 8           ; DRQ
-    jz .wait2
+;.wait2:
+  ;  in al, dx
+  ;  test al, 8           ; DRQ
+  ;  jz .wait2
 
-    ; ler dados
-    mov dx, 0x1F0
-    mov ecx, 256
+  ;  ; ler dados
+   ; mov dx, 0x1F0
+  ;  mov ecx, 256
 
-.read:
-    in ax, dx
-    mov [edi], ax
-    add edi, 2
-    loop .read
+;.read:
+  ;  in ax, dx
+  ;  mov [edi], ax
+  ;  add edi, 2
+   ; loop .read
 
-    popad
-    ret
+  ;  popad
+  ;  ret
 
 
 
@@ -92,6 +158,54 @@ load_fs:
 
 
 fs_find:
+
+    mov esi, buffer_fs
+    mov ecx, 64
+
+.next_entry:
+
+    cmp byte [esi], 0
+    je .not_found
+
+    push esi
+    push ecx
+
+    mov edi, shell_name
+    mov ecx, 12
+
+.compare:
+    mov al, [esi]
+    mov bl, [edi]
+    cmp al, bl
+    jne .fail
+
+    inc esi
+    inc edi
+    dec ecx
+    jnz .compare
+
+    ; encontrado
+    pop ecx
+    pop esi
+
+    mov al, [esi+12]
+    mov ah, [esi+13]
+    ret
+
+.fail:
+    pop ecx
+    pop esi
+
+    add esi, 16
+    dec ecx
+    jnz .next_entry
+
+.not_found:
+    xor ax, ax
+    ret
+
+
+fs_find_b:
 
     ;mov esi, 0x5000 ; buffer onde o FS foi carregado
     mov esi, buffer_fs
@@ -146,8 +260,11 @@ load_file:
     ; AH = SIZE
     ; EDI = destino
 
-    movzx eax, al
-    movzx ecx, ah
+    ;movzx eax, al
+    ;movzx ecx, ah
+
+    mov eax, 80
+    mov ecx, 10
 
 .loop:
     push eax
