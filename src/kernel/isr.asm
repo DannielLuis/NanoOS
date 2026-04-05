471f50d413fc4e34589d38b115d27b345fe926ca
Daniel Luis <danniellcontato@gmail.com>
2026-04-05 13:29:13 -0300

    adiciona syscalls reais

--- src/kernel/isr.asm
+++ src/kernel/isr.asm
@@ -14,15 +14,117 @@ isr1:
     push 1
     jmp isr_common
 
+
+; =========================
+; SYSCALL ISR (int 0x80)
+; =========================
+isr128:
+    cli
+
+    push 0          ; dummy error
+    push 128        ; interrupt number
+
+    jmp isr_common
+
 ; ... repete até 31
 
 isr_common:
     pusha
 
+    mov eax, [esp + 32]   ; int number (posição após pusha)
+
+    cmp eax, 128
+    je .syscall_handler
+
     call isr_handler
+    jmp .end
 
-    popa
-    add esp, 8
+    .syscall_handler:
+        call syscall_dispatch
 
-    sti
-    iret
+    .end:
+        popa
+        add esp, 8
+        sti
+        iret
+
+
+
+; =========================
+; SYSCALL DISPATCH
+; =========================
+syscall_dispatch:
+
+    ; eax = syscall number (já salvo no stack via pusha)
+    ; mas é preciso recuperar registradores originais
+
+    mov eax, [esp + 28]   ; eax original
+    mov ebx, [esp + 24]
+    mov ecx, [esp + 20]
+    mov edx, [esp + 16]
+    mov esi, [esp + 12]
+    mov edi, [esp + 8]
+
+    cmp eax, syscall_count
+    jae .invalid
+
+    mov edx, syscall_table
+    mov eax, [edx + eax*4]
+
+    call eax
+    jmp .done
+
+    .invalid:
+        mov eax, -1
+
+    .done:
+        mov [esp + 28], eax   ; retorno para caller
+        ret
+
+
+
+; =========================
+; SYSCALL TABLE
+; =========================
+syscall_table:
+    dd sys_write
+    dd sys_getpid
+    dd sys_exit
+
+syscall_count equ ($ - syscall_table) / 4
+
+
+
+; ===========================
+; Implementação das syscalls
+; ===========================
+sys_write:
+    ; ebx = ponteiro string
+
+.print:
+    mov al, [ebx]
+    cmp al, 0
+    je .done
+
+    mov ah, 0x0E
+    int 0x10
+
+    inc ebx
+    jmp .print
+
+.done:
+    mov eax, 0
+    ret
+
+
+
+sys_getpid:
+    mov eax, 1
+    ret
+
+
+sys_exit:
+.halt:
+    cli
+    hlt
+    jmp .halt
