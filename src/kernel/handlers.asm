833a24adad989767dfa6853608f10be103e83fd0
Daniel Luis <danniellcontato@gmail.com>
2026-04-05 14:48:57 -0300

    atualiza o Handlers de Interrupção

--- src/kernel/handlers.asm
+++ src/kernel/handlers.asm
@@ -7,8 +7,22 @@ isr_handler:
 
 irq_handler:
     ; EOI
+  ;  mov al, 0x20
+  ;  out 0x20, al
+  ;  out 0xA0, al
+
+  ;  ret
+
+    mov eax, [esp + 32]   ; int number
+
+    cmp eax, 40
+    jb .master_only
+
     mov al, 0x20
-    out 0x20, al
     out 0xA0, al
 
-    ret
+    .master_only:
+        mov al, 0x20
+        out 0x20, al
+
+        ret
