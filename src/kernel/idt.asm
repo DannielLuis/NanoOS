b96866b4a722d466d327f486f03dbe207da18347
Daniel Luis <danniellcontato@gmail.com>
2026-04-05 13:44:38 -0300

    altera para idt fixa na memoria

--- src/kernel/idt.asm
+++ src/kernel/idt.asm
@@ -1,33 +1,52 @@
 ; =========================
-; IDT
+; IDT (FIXA EM MEMÓRIA)
 ; =========================
 
-idt_start:
-times 256 dq 0
-idt_end:
+IDT_BASE equ 0x80000
 
 idt_descriptor:
-    dw idt_end - idt_start - 1
-    dd idt_start
+    dw 256*8 - 1        ; 256 entradas * 8 bytes
+    dd IDT_BASE         ; endereço fixo
 
+; ---------------------------------
+; ZERAR IDT (IMPORTANTE)
+; ---------------------------------
+idt_clear:
+    mov edi, IDT_BASE
+    mov ecx, 256*2      ; 256 entradas de 8 bytes = 512 dwords
+    xor eax, eax
+
+.clear_loop:
+    stosd
+    loop .clear_loop
+
+    ret
+
+; ---------------------------------
+; SETAR GATE
+; ---------------------------------
 idt_set_gate:
     ; eax = handler
     ; ebx = index
 
-    mov edx, idt_start
+    mov edx, IDT_BASE
     mov ecx, ebx
     shl ecx, 3
     add edx, ecx
 
-    mov word [edx], ax
-    mov word [edx + 2], 0x08
+    mov word [edx], ax          ; offset low
+    mov word [edx + 2], 0x08    ; code segment
     mov byte [edx + 4], 0
-    mov byte [edx + 5], 10001110b
+    mov byte [edx + 5], 10001110b ; present, ring0, interrupt gate
     shr eax, 16
-    mov word [edx + 6], ax
+    mov word [edx + 6], ax      ; offset high
 
     ret
 
+; ---------------------------------
+; INIT
+; ---------------------------------
 idt_init:
+    call idt_clear
     lidt [idt_descriptor]
     ret
